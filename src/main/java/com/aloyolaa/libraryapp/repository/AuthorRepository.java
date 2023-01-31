d36bf2068f30db51de430b82f4df402c7ba658eb
Alan Loyola <alanloyola2002@gmail.com>
2023-01-31 12:56:27 -0500

    feat: Added Base Repository

---
+++ src/main/java/com/aloyolaa/libraryapp/repository/AuthorRepository.java
@@ -0,0 +1,6 @@
+package com.aloyolaa.libraryapp.repository;
+
+import com.aloyolaa.libraryapp.entity.Author;
+
+public interface AuthorRepository extends BaseRepository<Author, Long> {
+}
--- src/main/java/com/aloyolaa/libraryapp/repository/CustomerRepository.java
+++ src/main/java/com/aloyolaa/libraryapp/repository/CustomerRepository.java
@@ -1,7 +1,6 @@
 package com.aloyolaa.libraryapp.repository;
 
 import com.aloyolaa.libraryapp.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends BaseRepository<Customer, Long> {
 }
