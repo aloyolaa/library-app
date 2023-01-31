dfb5d9eb47fe4e9971ac3e925e338277427d3b25
Alan Loyola <alanloyola2002@gmail.com>
2023-01-31 12:48:40 -0500

    feat: Added Author Entity

---
+++ src/main/java/com/aloyolaa/libraryapp/entity/Author.java
@@ -0,0 +1,22 @@
+package com.aloyolaa.libraryapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "author")
+public class Author extends BaseEntity {
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "biography", length = 1500)
+    private String biography;
+}
